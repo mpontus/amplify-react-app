cc905422271fceeecce65319602585b6722503ff
Michael Pontus <m.pontus@gmail.com>
2021-12-16 18:33:47 +0300

    `amplify add api`

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "amplifyreactapp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
